6ccae5b3068cb6155f69fe2bf8f9b2dc80ee8c84
Jackson Jessup <jessupjn@umich.edu>
2024-05-22 09:33:37 -0400

    job2base64string

---
+++ job2base64.ts
@@ -0,0 +1,31 @@
+import { OracleJob } from "@switchboard-xyz/common";
+import chalk from "chalk";
+
+// Serialize the jobs to base64 strings.
+const base64EncodeJob = (oracleJob: OracleJob) => {
+  const encoded = OracleJob.encodeDelimited(oracleJob).finish();
+  const base64 = Buffer.from(encoded).toString("base64");
+  return base64;
+};
+
+const print = (oracleJob: OracleJob) => {
+  console.log();
+  console.log(chalk.bold.yellowBright("Job:"));
+  console.log(JSON.stringify(oracleJob.toJSON(), null, 2));
+  console.log(chalk.bold.yellowBright("Encoded:"));
+  console.log(base64EncodeJob(oracleJob));
+};
+
+(() => {
+  const oracleJob = new OracleJob({
+    tasks: [{ valueTask: { value: 1337 } }],
+  });
+  print(oracleJob);
+})();
+
+(() => {
+  const oracleJob = new OracleJob({
+    tasks: [{ httpTask: { url: "https://google.com" } }],
+  });
+  print(oracleJob);
+})();
